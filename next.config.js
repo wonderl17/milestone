abe0ef44923831085dbb204c31454569f2819f6a
wonderl17 <809646531@qq.com>
2024-10-17 11:57:15 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform()
+}
+
 const nextConfig = {}
 
 module.exports = nextConfig
